8f3bf47fc0c22a2cd69956f5dabdf44430dd3639
Louise <101908312+louisekanizsai@users.noreply.github.com>
2024-02-01 16:33:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const yesBtn = createElement("button", "btn", "yes");
 const noBtn = createElement("button", "btn", "no");
 const img = createElement("img", "img");
 const hearts = createElement("div", "hearts");
-img.src = "../images/catwithheart.png";
+img.src = "https://github.com/louisekanizsai/be-my-valentine/blob/main/images/catwithheart.png";
 hearts.append(img);
 const buttonsContainer = document.querySelector(".btns-container");
 buttonsContainer.append(yesBtn, noBtn);
